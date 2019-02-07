5ec5b3e5ebdcd35cf9b4deaf8bec8b58d06cee33
Guilherme Bauer-Negrini <negrini.guilherme@gmail.com>
2019-02-07 09:25:40 -0200

    Check sequence lenght restriction

--- mirdb-custom-target-search.py
+++ mirdb-custom-target-search.py
@@ -16,7 +16,7 @@ fasta_file = 'SRA_circRNAs.uniq.fa'
 output_file = 'custom_target_search.xlsx'
 
 # Define number of sequences to search
-num_seq = 5
+num_seq = 50
 
 # Options for species: Human, Rat, Mouse, Chicken, Dog
 species = 'Human'
@@ -35,67 +35,68 @@ except IOError:
 firefox = webdriver.Firefox()
 targets_list = []
 for sequence in range(0, num_seq):
+    if 100 <= len(fasta[sequence]) <= 30000:  # miRDB range restriction
 
-    # Gets the desired page
-    firefox.get(url)
+        # Gets the desired page
+        firefox.get(url)
 
-    # Selects the options
-    select_species = Select(firefox.find_element_by_name('searchSpecies'))
-    select_species.select_by_visible_text(species)
-    select_submission = Select(firefox.find_element_by_name('subChoice'))
-    select_submission.select_by_visible_text(submission)
+        # Selects the options
+        select_species = Select(firefox.find_element_by_name('searchSpecies'))
+        select_species.select_by_visible_text(species)
+        select_submission = Select(firefox.find_element_by_name('subChoice'))
+        select_submission.select_by_visible_text(submission)
 
-    # Enters the sequence in the text field
-    sequence_field = firefox.find_element_by_name('customSub')
-    sequence_field.send_keys(fasta[sequence])
+        # Enters the sequence in the text field
+        sequence_field = firefox.find_element_by_name('customSub')
+        sequence_field.send_keys(fasta[sequence])
 
-    # Clicks on the "Go" button
-    firefox.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[3]/form/table/tbody/tr[5]/td/input[1]').click()
-    # Waits for the results
-    time.sleep(1)
-    # Clicks on the "Retrieve Prediction Result" button
-    firefox.find_element_by_xpath('/html/body/form/input[2]').click()
+        # Clicks on the "Go" button
+        firefox.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[3]/form/table/tbody/tr[5]/td/input[1]').click()
+        # Waits for the results
+        time.sleep(3)
+        # Clicks on the "Retrieve Prediction Result" button
+        firefox.find_element_by_xpath('/html/body/form/input[2]').click()
 
-    details = firefox.find_elements_by_name('.submit')  # the first is the "Return" button, the others are "Target Details"
+        details = firefox.find_elements_by_name('.submit')  # the first is the "Return" button, the others are "Target Details"
 
-    # loops through all targets
-    for i in range(1, len(details)):
-        details[i].click()
-        html = firefox.page_source
-        soup = BeautifulSoup(html, 'html.parser')  # parses the html with bs4 to scrape data from html tags
-        seeds = soup.find_all('font', {'color': '#0000FF'})  # find seeds by color
-        number_of_seeds = len(seeds)
-        links = soup.find_all('a', href=True)
-        mirna_link = 'www.mirdb.org'+links[1]['href']  # builds a link for the miRNA page
-        mirna_name = links[1].font.text  # gets miRNA name
+        # loops through all targets
+        for i in range(1, len(details)):
+            details[i].click()
+            html = firefox.page_source
+            soup = BeautifulSoup(html, 'html.parser')  # parses the html with bs4 to scrape data from html tags
+            seeds = soup.find_all('font', {'color': '#0000FF'})  # find seeds by color
+            number_of_seeds = len(seeds)
+            links = soup.find_all('a', href=True)
+            mirna_link = 'www.mirdb.org'+links[1]['href']  # builds a link for the miRNA page
+            mirna_name = links[1].font.text  # gets miRNA name
 
-        # usually the score is in cell #7, but sometimes there is an extra row for miRNA previous name
-        table = soup.find_all('td')
-        if table[7].text.isdigit():  # so check if cell #7 text is a digit
-            score = table[7].text
-        else:                       # if it isn't then score should be in cell #9
-            score = table[9].text
+            # usually the score is in cell #7, but sometimes there is an extra row for miRNA previous name
+            table = soup.find_all('td')
+            if table[7].text.isdigit():  # so check if cell #7 text is a digit
+                score = table[7].text
+            else:                       # if it isn't then score should be in cell #9
+                score = table[9].text
 
-        # stores the above information in a dictionary
-        targets_info = OrderedDict()  # OrderedDict() is used to preserve column order when creating dataframe
-        targets_info['sequence'] = fasta[sequence].id
-        targets_info['score'] = score
-        targets_info['#seeds'] = number_of_seeds
-        targets_info['mirna'] = mirna_name
-        targets_info['link'] = mirna_link
+            # stores the above information in a dictionary
+            targets_info = OrderedDict()  # OrderedDict() is used to preserve column order when creating dataframe
+            targets_info['sequence'] = fasta[sequence].id
+            targets_info['score'] = score
+            targets_info['#seeds'] = number_of_seeds
+            targets_info['mirna'] = mirna_name
+            targets_info['link'] = mirna_link
 
-        # adds the dictionary to a list of dictionaries containing the target's data
-        targets_list.append(targets_info)
+            # adds the dictionary to a list of dictionaries containing the target's data
+            targets_list.append(targets_info)
 
-        print('Sequence: {}'.format(fasta[sequence].id))
-        print('Number of seeds: {}'.format(number_of_seeds))
-        print('miRNA link: {}'.format(mirna_link))
-        print('miRNA name: {}'.format(mirna_name))
-        print('Score: {}'.format(score))
-        print('-------------------------------------')
+            print('Sequence: {}'.format(fasta[sequence].id))
+            print('Number of seeds: {}'.format(number_of_seeds))
+            print('miRNA link: {}'.format(mirna_link))
+            print('miRNA name: {}'.format(mirna_name))
+            print('Score: {}'.format(score))
+            print('-------------------------------------')
 
-        firefox.back()  # goes back to prediction page
-        details = firefox.find_elements_by_name('.submit')  # find all buttons again
+            firefox.back()  # goes back to prediction page
+            details = firefox.find_elements_by_name('.submit')  # find all buttons again
 
 dataframe = pd.DataFrame(targets_list)  # creates a pandas DataFrame("table") with targets information
 dataframe.to_excel(output_file, index=False)  # saves the dataframe to an Excel file
